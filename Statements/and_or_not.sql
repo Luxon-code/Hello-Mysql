90b089f199413981684141c4c82ff4e515901a42
Joseph Trujillo <jt9906157@gmail.com>
2024-02-02 11:19:19 -0500

    first commit

---
+++ Statements/and_or_not.sql
@@ -0,0 +1,5 @@
+SELECT * FROM users WHERE NOT email='santi@gmail.com';
+
+SELECT * FROM users WHERE NOT email='santi@gmail.com' AND age<20;
+
+SELECT * FROM users WHERE NOT email='santi@gmail.com' OR age=20;
---
+++ Statements/limit.sql
@@ -0,0 +1,3 @@
+SELECT * FROM users LIMIT 3;
+
+SELECT * FROM users WHERE NOT email='santi@gmail.com' OR age=20 LIMIT 2;
